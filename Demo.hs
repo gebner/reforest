7b27c5263411b8d71426066eaa74f9338a6bc9a2
Gabriel Ebner <gebner@gebner.org>
2015-03-25 18:16:12 +0100

    Perform rigid trigram merges.

--- Demo.hs
+++ Demo.hs
@@ -17,5 +17,17 @@ exampleLang = concatMap (\i -> map (r i) sq) [0..9]
     r i x = App (Con ("r" ++ show (i :: Int)) 1) [x]
     c = App (Con "c" 0) []
 
+exampleLang2 :: [Term]
+exampleLang2 = l 5
+  where
+    f x y = App (Con "f" 2) [x,y]
+    g x y = App (Con "g" 2) [x,y]
+    c = App (Con "c" 0) []
+    d = App (Con "d" 0) []
+
+    l :: Int -> [Term]
+    l 0 = [c,d]
+    l i = do { t <- l (i-1); [f t t, g t t] }
+
 main :: IO ()
-main = mapM_ print $ sort $ findTratGrammar exampleLang
+main = mapM_ print $ sort $ findTratGrammar exampleLang2
--- src/Reforest.hs
+++ src/Reforest.hs
@@ -57,49 +57,72 @@ type Grammar = [Production]
 prods :: NonTerm -> Grammar -> Grammar
 prods a = filter ((== a) . lhs)
 
-data Digram = Digram Sym Int Sym
+data Ngram = Digram Sym Int Sym | RigidTrigram Sym Int Int
             deriving (Eq, Ord, Show)
 
-listDigrams' :: Term -> [Digram] -> [Digram]
+listDigrams' :: Term -> [Ngram] -> [Ngram]
 listDigrams' (App s xs) = localDigrams 0 xs
   where
     localDigrams _ [] = id
     localDigrams i (y@(App (Bnd _) _) : ys) = listDigrams' y . localDigrams (i+1) ys
     localDigrams i (y@(App r _) : ys) = listDigrams' y . (Digram s i r :) . localDigrams (i+1) ys
 
-listDigrams :: Grammar -> [Digram]
+listDigrams :: Grammar -> [Ngram]
 listDigrams = foldr (listDigrams' . rhs) []
+
+listRigidTrigrams' :: Term -> [Ngram] -> [Ngram]
+listRigidTrigrams' (App s xs) rest =
+    [ RigidTrigram s i j |
+        (i, App r1 _) <- zip [0..] xs,
+        (j, App r2 _) <- zip [0..] xs,
+        i < j, r1 == r2
+    ] ++ foldr listRigidTrigrams' rest xs
+
+listRigidTrigrams :: Grammar -> [Ngram]
+listRigidTrigrams = foldr (listRigidTrigrams' . rhs) []
 
 frequencies :: Ord a => [a] -> [(a, Int)]
 frequencies = map (head &&& length) . group . sort
 
-sortedDigrams :: Grammar -> [(Digram, Int)]
-sortedDigrams = sortBy (flip compare `on` snd) . frequencies . listDigrams
+sortedNgrams :: Grammar -> [(Ngram, Int)]
+sortedNgrams g = sortBy (flip compare `on` snd) . frequencies $ listDigrams g ++ listRigidTrigrams g
 
 maxNonTermNo :: Grammar -> Int
 maxNonTermNo = maximum . map (\(Prod (NT i _) _) -> i)
 
-abbreviateDigram :: Digram -> Grammar -> Grammar
-abbreviateDigram d@(Digram s i r) g = newProd : map abbrProd g
+abbreviateNgram :: Ngram -> Grammar -> Grammar
+abbreviateNgram d@(Digram s i r) g = newProd : map abbrProd g
     where
       newNT = NT (maxNonTermNo g + 1) (arity s + arity r - 1)
       newProd = Prod newNT (App s sArgs)
       sArgs = [ App (Bnd j) [] | j <- [0..i-1] ] ++ [App r rArgs] ++ [ App (Bnd (j+arity r-1)) [] | j <- [i+1..arity s-1] ]
       rArgs = [ App (Bnd (i+j)) [] | j <- [0..arity r-1] ]
-      abbrProd (Prod a t) = Prod a (abbreviateDigramBy d newNT t)
+      abbrProd (Prod a t) = Prod a (abbreviateNgramBy d newNT t)
+abbreviateNgram ngram@(RigidTrigram s i j) g = newProd : map abbrProd g
+    where
+      newNT = NT (maxNonTermNo g + 1) (arity s - 1)
+      newProd = Prod newNT (App s sArgs)
+      sArgs = [ App (Bnd k) [] | k <- [0..j-1] ] ++ [App (Bnd i) []] ++ [ App (Bnd (k-1)) [] | k <- [j+1..arity s-1] ]
+      abbrProd (Prod a t) = Prod a (abbreviateNgramBy ngram newNT t)
 
-abbreviateDigramBy :: Digram -> NonTerm -> Term -> Term
-abbreviateDigramBy d@(Digram s i r) nt (App f xs)
-  | f == s && rootSym (xs !! i) == r = abbreviateDigramBy d nt $
+abbreviateNgramBy :: Ngram -> NonTerm -> Term -> Term
+abbreviateNgramBy d@(Digram s i r) nt (App f xs)
+  | f == s && rootSym (xs !! i) == r = abbreviateNgramBy d nt $
       App (Var nt) $ [ xs !! j | j <- [0..i-1] ] ++ args (xs !! i) ++ [ xs !! j | j <- [i+1..arity s-1] ]
-  | otherwise = App f (map (abbreviateDigramBy d nt) xs)
+  | otherwise = App f (map (abbreviateNgramBy d nt) xs)
+abbreviateNgramBy d@(RigidTrigram s i j) nt (App f xs)
+  | f == s && xs !! i == xs !! j = abbreviateNgramBy d nt $
+      App (Var nt) [ xs !! k | k <- [0..arity s-1], k /= j ]
+  | otherwise = App f (map (abbreviateNgramBy d nt) xs)
 
 abbreviateMostCommonDigram :: Grammar -> Grammar
-abbreviateMostCommonDigram g = abbreviateDigram (fst $ head $ sortedDigrams g) g
+abbreviateMostCommonDigram g = abbreviateNgram (fst $ head $ sortedNgrams g) g
 
-digramCompression :: Grammar -> Grammar
-digramCompression g = if f == 1 then g else digramCompression (abbreviateDigram dg g)
-    where ((dg, f) : _) = sortedDigrams g
+ngramCompression :: Grammar -> Grammar
+ngramCompression g
+      | f > 1 = ngramCompression (abbreviateNgram dg g)
+      | otherwise = g
+    where ((dg, f) : _) = sortedNgrams g
 
 extractSimpleA0Rule :: Production -> Maybe (Sym, Sym)
 extractSimpleA0Rule p
@@ -179,14 +202,14 @@ tryIntroNT g =
 nTryIntroNT :: Grammar -> Grammar
 nTryIntroNT g =
     case tryIntroNT g of
-      Just g' -> nTryIntroNT $ simpl $ digramCompression g'
+      Just g' -> nTryIntroNT $ simpl $ ngramCompression g'
       Nothing -> g
 
 langToGrammar :: [Term] -> Grammar
 langToGrammar = map (Prod (NT 0 0))
 
 findTratGrammar :: [Term] -> Grammar
-findTratGrammar = simplFull . nTryIntroNT . digramCompression . langToGrammar
+findTratGrammar = simplFull . nTryIntroNT . ngramCompression . langToGrammar
 
 subterms :: Term -> [Term]
 subterms t@(App _ xs) = t : concatMap subterms xs
